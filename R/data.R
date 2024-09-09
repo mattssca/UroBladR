55c4235404895a799127a1d52f0325ccc1196a71
mattssca <mattsada@gmail.com>
2024-09-09 08:10:32 +0200

    updating new example dataset, adding metadata

--- R/data.R
+++ R/data.R
@@ -1,6 +1,5 @@
-#' isoploteR
-#' Takes an expression matrix and subsets to sample IDs of interest for the selected genes/isoforms.
-#' Additionally, the function can also return a basic box plot with the returned data.
+#' UroBladR
+#' A collection of analysis tools in R, directed towards research in the urothelial cancer field.
 #'
 "_PACKAGE"
 
@@ -23,29 +22,76 @@
 #' @format A data frame with 252894 rows (genes) and 3 columns (different gene formats).
 NULL
 
-#' Expression Matrix Subset.
+#' SWOG Expressions.
 #'
-#' Example expression matrix subset to only include 10 samples.
+#' Example gene expression dataset. Obtained from the Southwest Oncology Group (SWOG).
+#' doi: 10.1158/1078-0432.CCR-23-0602.
 #'
-#' A data frame with samples in columns and expression values in rows (TPM).
+#' A data frame with samples in columns and expression values in rows with gene
+#' names (HGNC symbols) as row names.
 #'
 #' \itemize{
-#'  \item entrz_id. Isoform annotation in Entrez format.
-#'  \item X18KFU0001. Sample 1
-#'  \item X18KFU0002. Sample 2
-#'  \item X18KFU0003. Sample 3
-#'  \item X18KFU0004. Sample 4
-#'  \item X18KFU0006. Sample 5
-#'  \item X18KFU0007. Sample 6
-#'  \item X18KFU0008. Sample 7
-#'  \item X18KFU0009. Sample 8
-#'  \item X18KFU00010. Sample 9
-#'  \item X18KFU00012. Sample 10
 #' }
 #'
 #' @docType data
 #' @keywords datasets
-#' @name expression_sub
-#' @usage data(expression_sub)
-#' @format A data frame with 252894 rows (genes) and 11 columns.
+#' @name swog_exp
+#' @usage data(swog_exp)
+#' @format A data frame with 15986 rows (genes) and 163 columns (samples).
+NULL
+
+#' SWOG Metadata.
+#'
+#' Metadata associated with the bundled expression dataset.Obtained from the
+#' Southwest Oncology Group (SWOG). doi: 10.1158/1078-0432.CCR-23-0602.
+#'
+#' A data frame with samples in rows and metadata variables in the columns.
+#'
+#' \itemize{
+#'  \item ALTPATID. Unique sample ID.
+#'  \item ARMNAME.
+#'  \item STRAT1.
+#'  \item STRAT2.
+#'  \item AGE.
+#'  \item SEX.
+#'  \item RACE.
+#'  \item prim_analysis.
+#'  \item itt_analysis.
+#'  \item adequate_tiss.
+#'  \item ANY_TRT.
+#'  \item GFS.
+#'  \item adewuate_chemo.
+#'  \item CYSTECTOMY.
+#'  \item PATHSTHT.
+#'  \item PATHSTGN.
+#'  \item PATHSTGM.
+#'  \item NLYMPHN.
+#'  \item NPOSNOD.
+#'  \item PATH_RESP.
+#'  \item pT0.
+#'  \item OS_IND.
+#'  \item OS_TIM.
+#'  \item PFS_IND.
+#'  \item PFS_TIM.
+#'  \item TT_offtx.
+#'  \item TT_cyst.
+#'  \item GC_FAV.
+#'  \item ddMVAC_FAV.
+#'  \item GC_FAV_dup.
+#'  \item ddMVAC_FAV_dup.
+#'  \item GEO.
+#'  \item tchagrp.
+#'  \item tcga.
+#'  \item LundTax2023_simple.
+#'  \item consensus.
+#'  \item mda.
+#'  \item mdagrp.
+#'  \item poor_quality.
+#' }
+#'
+#' @docType data
+#' @keywords datasets
+#' @name swog_meta
+#' @usage data(swog_meta)
+#' @format A data frame with 163 rows (samples) and 39 columns (metadata).
 NULL
